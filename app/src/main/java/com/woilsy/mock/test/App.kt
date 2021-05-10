051aa0cc7983a8859ae8e684c4781d7b55a7511c
leo <635406113@qq.com>
2021-05-11 00:14:22 +0800

    最新测试案例

--- app/src/main/java/com/woilsy/mock/test/App.kt
+++ app/src/main/java/com/woilsy/mock/test/App.kt
@@ -5,7 +5,6 @@ import com.woilsy.mock.MockLauncher
 import com.woilsy.mock.entity.MockObj
 import com.woilsy.mock.options.MockOptions
 import com.woilsy.mock.test.api.ApiService
-import com.woilsy.mock.test.api.ApiService2
 import com.woilsy.mock.test.http.HttpManager
 import com.woilsy.mock.type.MockStrategy
 
@@ -22,7 +21,6 @@ class App : Application() {
                 .setBackupBaseUrl("http://www.baidu.com")
                 .build(),
             MockObj(ApiService::class.java, MockStrategy.RESOLVE_WITH_EXCLUDE),
-            MockObj(ApiService2::class.java, MockStrategy.RESOLVE_NOT_WITH_INCLUDE)
         )
         //初始化http
         HttpManager.init(this, MockOptions.BASE_URL)
