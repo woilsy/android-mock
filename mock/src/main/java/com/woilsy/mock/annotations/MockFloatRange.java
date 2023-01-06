73a87632d1868918464223aa3c9cb20dcc1ccd50
leo <leo@woilsy.com>
2023-01-06 19:21:28 +0800

    完善注释

--- mock/src/main/java/com/woilsy/mock/annotations/MockFloatRange.java
+++ mock/src/main/java/com/woilsy/mock/annotations/MockFloatRange.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 /**
  * value: choose one at random
- * from&to: [from,to]
+ * from&to: [from,to)
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
